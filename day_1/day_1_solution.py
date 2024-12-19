8b5374d08bc996a448869c4431420bc2779c7c5c
Jeffrey King <jeffrey.king@trinityis.com>
2024-12-19 07:39:28 -0800

    day 2

--- day_1/day_1_solution.py
+++ day_1/day_1_solution.py
@@ -1,4 +1,4 @@
-print('Welcome to the band name generator')
+print('Welcome to the band name generator.')
 user_selected_city = input("What's the name of the city you grew up in?\n")
 user_selected_pet_name = input("What's your pet's name?\n")
-print('Your band name could be ' + user_selected_city + ' ' + user_selected_pet_name)
+print('Your band name could be: ' + user_selected_city + ' ' + user_selected_pet_name)
