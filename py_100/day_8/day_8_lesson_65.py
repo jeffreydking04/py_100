fe7e575ce513fe43a13f4cf9c80ed870e8f2b8ad
Jeffrey King <jeffrey.king@trinityis.com>
2024-12-30 05:59:45 -0800

    day 2 of day 8; I am so slow

--- py_100/day_8/day_8_lesson_65.py
+++ py_100/day_8/day_8_lesson_65.py
@@ -5,122 +5,90 @@
 # handing us the project, just like hangman.  I will likely just hit the project then watch the 
 # videos and make notes and adjustments.
 
-# 2 step imports 3
-import os # almost always for terminal display, just keeps things tidy to clear screen between steps
+import os
 
-# 3 step constants 4
-messages = {
-    'intro': 'Welcome to Caesar Cipher! Would you like to encode or decode?',
-    'encode_prompt': 'Type the message you would like to encode:',
-    'decode_prompt': 'Type the message you would like to decode:',
-    'shift_prompt': 'Enter the shift amount:',
-    'encode_result': 'Your encoded message:',
-    'decode_result': 'Your decoded message:',
-    'invalid_input': 'Invalid Input'
-}
+small_letters = 'abcdefghijklmnopqrstuvwxyz'
+large_letters = small_letters.upper()
 
-# 4 step state 4.5
 state = {
-    'mode': '', # 'encode' or 'decode' or 'q'
-    'input': '',
-    'output': '', # whether encoding or decoding, we are going to have a string input and a string output
-    'message': 'intro',
-    'invalid': False
+    'user_input': ''
 }
-
-def mode():
-    return state['mode']
 
 def user_input():
-    return state['input']
+    return state['user_input']
 
-def output():
-    return state['output']
+def set_user_input(val):
+    state['user_input'] = val
 
-def set_mode(mode):
-    state['mode'] = mode
+def shift_letter(l, shift):
+    if l in small_letters:
+        idx = (small_letters.index(l) + shift) % 26
+        return small_letters[idx]
+    
+    idx = (large_letters.index(l) + shift)
+    # because I didn't know you could do this:
+    idx %= 26
+    return large_letters[idx]
 
-def message():
-    return state['message']
+def encode_message(shift):
+    shifted_message = ''
+    for l in user_input():
+        if l != ' ':
+            shifted_message += shift_letter(l, int(shift))
+        else:
+            shifted_message += ' '
+    return shifted_message
 
-def set_message(message):
-    state['message'] = message
+def decode_message(shift):
+    shifted_message = ''
+    for l in user_input():
+        if l != ' ':
+            shifted_message += shift_letter(l, -int(shift))
+        else:
+            shifted_message += ' '
+    return shifted_message
 
-def invalid():
-    return state['invalid']
-
-def set_invalid(invalid):
-    state['invalid'] = invalid
-
-def set_input(input):
-    state['input'] = input
-
-def set_output(output):
-    state['output'] = output
-
-def display_message():
-    input(f'{messages[message()]}\n\n')
-
-def valid_mode():
-    if input() in ['encode', 'decode', 'q']:
-        return True
-    return False
-
-# 4.5 step display 5
-    # I knew I should be spacing out my node numbers as a best practice
-while mode() != 'q':
+def print_invalid_input():
     os.system('clear')
-    if invalid():
-        print(messages['invalid_input'])
-        set_invalid(False)
-    user_response = display_message()
+    print('Invalid Input!')
 
-# 5 step input_encode_decode 6
-    if message() == 'intro':
-        print('Here')
-        if not valid_mode():
-            set_invalid(True)
-            continue
-        if mode() == 'q':
-            print('Goodbye!\n\n')
-            break
-        if mode() == 'encode':
-            set_message('encode_prompt')
-            continue
-        set_message('decode_prompt')
+def get_shift():
+    return input('What shift?\n\n')
+
+os.system('clear')
+ 
+print('Welcome to Caesar Cipher!')
+ 
+while not user_input() == 'q':
+    set_user_input(input('Would you like to encode, decode, or q?\n\n').lower())
+    if user_input() not in ['encode', 'decode', 'q']:
+        print_invalid_input()
         continue
-        
-    set_mode('q')
-
-# 7 split is_5_valid 8 9
-
-# 8 step 5_valid 10
-
-# 9 step 5_invalid 5
-
-# 10 step input_message 11
-
-# 11 split is_10_valid 12 13
-
-# 12 step 10_valid 14
-
-# 13 step 10_invalid 10
-
-# 14 step input_shift 15
-
-# 15 split is_14_valid 16 17
-
-# 16 step 14_valid 18
-
-# 17 step 14_invalid 14
-
-# 18 split e_or_d 19 20
-
-# 19 step encode 21
-
-# 20 step decode 22
-
-# 21 end display_e 0
-
-# 22 end display_d 0
-
+    if user_input() == 'q':
+        os.system('clear')
+        print('Goodbye!\n\n')
+        continue
+    if user_input() == 'encode':
+        set_user_input(input('Enter message to encode:\n\n'))
+        while not user_input().isalpha():
+            print_invalid_input()
+            set_user_input(input('Enter message to encode:\n\n'))
+        shift = get_shift()
+        while not shift.isnumeric():
+            print_invalid_input()
+            shift = get_shift()
+        print('The encoded message is:', encode_message(shift))
+        input('Enter any key to continue: \n\n')
+        os.system('clear')
+    else:
+        set_user_input(input('Enter message to decode:\n\n'))
+        while not user_input().isalpha():
+            print_invalid_input()
+            set_user_input(input('Enter message to decode:\n\n'))
+        shift = get_shift()
+        while not shift.isnumeric():
+            print_invalid_input()
+            shift = get_shift()
+        print('The decoded message is:', decode_message(shift))
+        input('Enter any key to continue: \n\n')
+        os.system('clear')
