d0a19f0472061d025c355dc8f7dbe106dcaafead
Jeffrey King <jeffrey.king@trinityis.com>
2025-01-02 03:57:37 -0800

    finish up day 9

--- py_100/day_9/day_9_lesson_70_secret_auction.py
+++ py_100/day_9/day_9_lesson_70_secret_auction.py
@@ -1,12 +1,48 @@
 # Day 8 Lesson 70: Secret Auction
 
-# What is on the screen:
+import os
+
+def clear_screen():
+    os.system('clear')
+
+bids = {}
+# Every time I don't put state in a dictionary, it bites me.
+state = {
+    'keep_going': True
+}
+
+def keep_going():
+    return state['keep_going'] 
+
+def set_keep_going(val):
+    state['keep_going'] = val 
+
+def populate_bid():
+    clear_screen()
+    # prompt for name
+    name = input('What is your name?\n\n')
+    # prompt for bid
+    bid = int(input('\n\nWhat is your bid?\n\n$'))
+    bids[name]= bid
+    # prompt for other bidders (are there any y/n)
+    # We could handle invalid input here, but bigger fish to fry.
+    set_keep_going(input('\n\nAre there any other bidders?\n\n') == 'y')
 
 # welcome message
-# prompt for name
-# prompt for bid
-# prompt for other bidders (are there any y/n)
+clear_screen()
+print('Welcome to the Secret Auction')
 
 # if yes: then clear screen and repeat
+while keep_going() == True:
+    populate_bid()
+
+max_bid = 0
+max_name = ''
+for name, bid in bids.items():
+    if bid > max_bid:
+        max_bid = bid
+        max_name = name
 
 # if no: display the winner
+clear_screen()
+print(f'The winner of the Secret Auction is {max_name}, with a bid of ${max_bid}.\n\n')
